5baec2bd9991f62d1a6e45d1185911b55c143654
Igor Dmitriev <igor.s.dmitriev@gmail.com>
2017-01-10 10:46:50 -0800

    Add backend auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,52 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                  :integer          not null, primary key
+#  email               :string
+#  timezone            :string           default("UTC")
+#  password_digest     :string
+#  session_token       :string
+#  image_url           :string
+#  default_travel_mode :string           default("driving")
+#  home_address        :string
+#  home_lat            :float
+#  home_lng            :float
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#
+
 class User < ApplicationRecord
+  validates :username, :session_token, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+  validates :password, length: { minimum: 6 }, allow_nil: true
+  validates :email, length: { minimum: 6 }
+
+  attr_reader :password
+
+  after_initialize :ensure_session_token
+  
+  def self.find_by_credentials(username, password)
+    user = User.find_by_username(username)
+    user && user.is_password?(password) ? user : nil
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom::urlsafe_base64(128)
+    self.save
+    self.session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom::urlsafe_base64(128)
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                  :integer          not null, primary key
+#  email               :string
+#  timezone            :string           default("UTC")
+#  password_digest     :string
+#  session_token       :string
+#  image_url           :string
+#  default_travel_mode :string           default("driving")
+#  home_address        :string
+#  home_lat            :float
+#  home_lng            :float
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#
+
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
